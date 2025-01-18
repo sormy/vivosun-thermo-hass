a501eae6f0475fc7eb92528132e87bb751088d23
Artem Butusov <art.sormy@gmail.com>
2025-01-18 11:34:39 -0500

    Fix sensor read logic

--- src/custom_components/vivosun_thermo/sensor.py
+++ src/custom_components/vivosun_thermo/sensor.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime
 from decimal import Decimal
+from logging import getLogger
 from typing import override
 
 from homeassistant.components.sensor import SensorEntity
@@ -45,19 +46,15 @@ class VivosunThermoSensor(CoordinatorEntity, SensorEntity):
         sensor_info = SENSOR_TYPES[sensor_type]
         device_info = DEVICE_TYPES.get(coordinator.discovery_info.name, {})
 
-        entity_name = coordinator.name
-
         device_name = device_info.get("name", coordinator.discovery_info.name)
         device_manufacturer = device_info.get("manufacturer")
         device_model = device_info.get("model")
 
-        self._attr_name = f"{entity_name} {probe_type.capitalize()} {sensor_info['name']}"
+        self._attr_name = f"{coordinator.name} {probe_type.capitalize()} {sensor_info['name']}"
         self._attr_icon = sensor_info["icon"]
         self._attr_device_class = sensor_info["device_class"]
         self._attr_state_class = sensor_info["state_class"]
         self._attr_entity_category = sensor_info["entity_category"]
-        self._attr_native_value = None
-        self._attr_available = False
         self._attr_native_unit_of_measurement = sensor_info["native_unit_of_measurement"]
         self._attr_suggested_display_precision = sensor_info["precision"]
         self._attr_unique_id = f"{coordinator.discovery_info.name}-{coordinator.discovery_info.address}-{probe_type}-{sensor_type}"
@@ -69,28 +66,13 @@ class VivosunThermoSensor(CoordinatorEntity, SensorEntity):
             model=device_model,
         )
 
-        self._update_native_value()
-        self._update_available()
-
-    def _update_native_value(self) -> None:
-        probe_data = self.coordinator.data.get(self.probe_type, {})
-        self._attr_native_value = probe_data.get(self.sensor_type)
-
-    def _update_available(self) -> None:
-        self._attr_available = self.coordinator.data.get(self.probe_type) is not None
-
     @property
     @override
     def native_value(self) -> StateType | date | datetime | Decimal:  # type: ignore
-        return self._attr_native_value
+        probe_data = self.coordinator.data.get(self.probe_type, {})
+        return probe_data.get(self.sensor_type)
 
     @property
     @override
     def available(self) -> bool:  # type: ignore
-        return super().available and self._attr_available
-
-    @override
-    async def async_update(self) -> None:
-        await super().async_update()
-        self._update_native_value()
-        self._update_available()
+        return self.coordinator.data.get(self.probe_type) is not None
